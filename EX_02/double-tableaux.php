a1b5d465c0e6ce696665b9e31d996288010d4147
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-07-02 15:15:04 +0200

    jour 7

---
+++ EX_02/double-tableaux.php
@@ -0,0 +1,18 @@
+<?php
+
+$panier= [
+    ['T-shirt rouge', 15.50, 5],
+    ['T-shirt vert', 15.50, 6],
+    ['T-shirt argent', 15.50, 8],
+    ['Short bleu', 16.50, 5],
+    ['Short vert', 19.99, 5],
+    ['Veste argent', 19.99, 10],
+    ['Veste argent', 35, 3],
+];
+function afficher_panier($panier)
+{
+
+    return;
+}
+afficher_panier($panier)
+?>
